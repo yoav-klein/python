83b78d26e1690048ef97062d2342929f9057e58d
YOAV KLAIN <YOAVKL@rafael.co.il>
2021-06-29 13:23:02 +0300

    Arg parses

--- topics/arg_parse/argparse_ex.py
+++ topics/arg_parse/argparse_ex.py
@@ -2,10 +2,12 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description="desc")
+os = "linux"
+parser = argparse.ArgumentParser(description="create a new repository")
 
-parser.add_argument("repo_name")
-if(os.)
+parser.add_argument("repo_name", help="The name of the new repository")
+if(os == "linux"):
+    parser.add_argument("token", help="In linux, enter an Access Token")
 args = parser.parse_args()
 
 print(args.repo_name)
