54b30afabd125ea505b8bb50a43271720ff463a9
YOAV KLAIN <YOAVKL@rafael.co.il>
2021-06-29 13:01:13 +0300

    topics

---
+++ topics/arg_parse/argparse_ex.py
@@ -0,0 +1,11 @@
+
+
+import argparse
+
+parser = argparse.ArgumentParser(description="desc")
+
+parser.add_argument("repo_name")
+if(os.)
+args = parser.parse_args()
+
+print(args.repo_name)
---
+++ topics/subprocess/Script.ps1
@@ -0,0 +1,4 @@
+
+
+Write-Error "I'm a failing script man !"
+exit 1
