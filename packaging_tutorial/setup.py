33ebc40c1ab22c26e4a1f3be0ac8597d8a5aefa1
yoavkl <YOAVKL@rafael.co.il>
2021-07-19 18:18:43 +0300

    Packaging tutorial

---
+++ packaging_tutorial/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+print("Calling setuptools.setup")
+
+setuptools.setup(
+    name="example-pkg-yoav-klein",
+    version="0.0.1",
+    author="Yoav Klein",
+    author_email="yoavklein25@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
