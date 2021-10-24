010731c0bcfc4d54f0d2817232db2cbcf2241e21
Yoav <yoavklein25@gmail.com>
2021-10-24 23:21:02 +0300

    Keep going with the new validate_structure

--- toolbox/validate_structure/split_path.py
+++ toolbox/validate_structure/split_path.py
@@ -2,38 +2,19 @@
 import os.path
 import re
 
-path = os.path.normpath("foo/bar/baz")
-path_components = path.split(os.sep)
-current = os.getcwd()
 
 def search_rec(current_dir, path_pattern, search_pattern):
-    print("-----------")
-    path_pattern = os.path.normpath(path_pattern)
-    path_pattern_components = path_pattern.split(os.sep)
+    first_part_in_path_pattern = path_pattern.split("/", 1)[0]
     
-    print(f"Current path: {current_dir}")
-    print(f"Path pattern: {path_pattern}")
-    
-    current_pattern = path_pattern.split(os.sep, 1)[0]
-    
-    print(f"Current pattern: {current_pattern}")
-    print(f"path_pattern_components: {len(path_pattern_components)}")
-    
-    if len(path_pattern_components) > 1:
-        next_pattern = path_pattern.split(os.sep, 1)[1]
-        print(f"Next pattern: {next_pattern}")
-        print(f"Current dir contents: {os.listdir(current_dir)}")
-        subdirs = [dir for dir in os.listdir(current_dir)] #  if re.search(current_pattern, dir) and os.path.isdir(dir)
-        matching_dirs = []
-        for dir in subdirs:
-            search_res = re.search(current_pattern, dir)
-            print(f"dir: {dir}: {re.search(current_pattern, dir)}")
-            if(search_res):
-                matching_dirs.append(dir)
-        #print(f"Matching subdirs: {subdirs}")
+    if len(path_pattern.split("/")) > 1:
+        matching_subdirs = [dir for dir in os.listdir(current_dir) \
+            if os.path.isdir(os.path.join(current_dir, dir)) \
+            and re.search(first_part_in_path_pattern, dir)] 
+        
         sum = 0
-        for subdir in matching_dirs:
-            result = search_rec(os.path.join(current_dir, subdir), next_pattern, search_pattern)
+        rest_of_path_pattern = path_pattern.split("/", 1)[1]
+        for subdir in matching_subdirs:
+            result = search_rec(os.path.join(current_dir, subdir), rest_of_path_pattern, search_pattern)
             if -1 == result:
                 return -1
             sum += result
@@ -41,15 +22,26 @@ def search_rec(current_dir, path_pattern, search_pattern):
         return sum
 
     else:
-        print(f"Dir contents: {os.listdir(current_dir)}")
-        files = [file for file in os.listdir(current_dir) if re.search(current_pattern, file) and os.path.isfile(os.path.join(current_dir, file))]
-        print(f"Found files: {files}")
+        files = [file for file in os.listdir(current_dir) \
+            if re.search(first_part_in_path_pattern, file) \
+            and os.path.isfile(os.path.join(current_dir, file))]
+
         for file in files:
-            print(f"Searching in {path_pattern}")
-        return 0
+            found = False
+            print(f"Searching in {os.path.join(current_dir, file)}")
+            with open(os.path.join(current_dir, file)) as f:
+                for line in f:
+                    if re.search(search_pattern, line):
+                        found = True
+            if found == False:
+                return -1
+
+        return 1
 
 def search_pattern(path, pattern):
     result = search_rec(".", path, pattern)
+    return result
     
 
-search_pattern('ba.+/fo.+/ben.txt', 'somepattern')
+res = search_pattern('design/m_.+/.+_init.m', 'somepattern')
+print(f"res: {res}")
