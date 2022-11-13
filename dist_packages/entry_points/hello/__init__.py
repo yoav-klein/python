25cf752b53f32b2914cd6588aa484500cb278150
Yoav Klein <yoavkl@rafael.co.il>
2022-11-13 11:06:49 +0200

    removing unnecessary file

--- dist_packages/entry_points/hello/__init__.py
+++
@@ -1,6 +0,0 @@
-
-
-print("top level code in __init__.py is executed")
-
-def hello_world():
-    print("Hello world")
--- dist_packages/entry_points/hello/__main__.py
+++
@@ -1,8 +0,0 @@
-
-from . import hello_world
-
-print("top level code in __main__.py is executed")
-
-if __name__ == "__main__":
-    hello_world()
-    
